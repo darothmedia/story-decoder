4056687db57ad05419941edb9161646e5e97c6e7
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-01-12 14:42:11 -0800

    start user routing

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,4 +12,39 @@ router.get('/', (req, res) => res.json({
   message: 'You are now at the user route'
 }))
 
+// Registration & Login
+router.post('/submit', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        //log the user in
+
+        //add user details if provided
+        if (req.body.name) {
+          user.name = req.body.name
+          user.save()
+        };
+        if (req.body.emoji) {
+          user.emoji = req.body.emoji
+          user.save()
+        };
+
+        //welcome the user back
+        res.json({
+          message: `Welcome back, ${user.name ? user.name : user.email}!`
+        })
+      } else {
+        //create a new user
+        const newUser = new User({
+          email: req.body.email,
+          name: req.body.name,
+          emoji: req.body.emoji
+        });
+        newUser.save()
+          .then(user => res.json(user))
+          .catch(err => console.log(err))
+      }
+    })
+})
+
 module.exports = router;
