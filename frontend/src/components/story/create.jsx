2cbe9535aa274c35c909c88f6a8502bb79534d4d
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-01-18 22:05:00 -0800

    cleanup errors

--- frontend/src/components/story/create.jsx
+++ frontend/src/components/story/create.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const CreateStory = props => {
   return(
-    <div class="wrapper" id="createwrapper">
+    <div className="wrapper" id="createwrapper">
       <form action="">
         <label>Start with a title for your story!
           <input type="text" />
