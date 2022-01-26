2ccde4a3d1e2a98eb3a0a985e2b2bb0050eb0717
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-01-25 23:16:25 -0800

    session actions and reducer

---
+++ frontend/src/actions/session_actions.js
@@ -0,0 +1,40 @@
+import * as SessionAPIUtil from '../util/session_api_util'
+import jwt_decode from 'jwt-decode'
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER'
+export const LOGOUT = 'LOGOUT'
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS'
+
+export const logoutUser = () => ({
+  type: LOGOUT
+});
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+})
+
+export const logout = () => dispatch => {
+  localStorage.removeItem('jwtToken')
+  SessionAPIUtil.setAuthToken(false)
+  dispatch(logoutUser())
+};
+
+export const submit = user => dispatch => {
+  SessionAPIUtil.login(user)
+    .then(res => {
+      const { token } = res.data;
+      localStorage.setItem('jwtToken', token);
+      SessionAPIUtil.setAuthToken(token);
+      const decoded = jwt_decode(token);
+      dispatch(receiveCurrentUser(decoded))
+    })
+    .catch(err => {
+      dispatch(receiveErrors(err.response.data))
+    })
+}
