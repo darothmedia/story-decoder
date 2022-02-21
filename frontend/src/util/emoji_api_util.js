77bb7014c9d304916e2013731622311b7a62e92f
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-02-21 13:45:22 -0800

    add api category access

--- frontend/src/util/emoji_api_util.js
+++ frontend/src/util/emoji_api_util.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { emojiKey } from "../config/keys";
 
-export const searchEmojis = searchTerm => {
-  return(
-    axios.get(`https://emoji-api.com/emojis?search=${searchTerm}&access_key=${emojiKey}`)
-  )
-}
+export const searchEmojis = searchTerm => (
+  axios.get(`https://emoji-api.com/emojis?search=${searchTerm}&access_key=${emojiKey}`)
+)
+
+export const searchByCategory = category => (
+  axios.get(`https://emoji-api.com/categories/${category}&access_key=${emojiKey}`)
+)
 
