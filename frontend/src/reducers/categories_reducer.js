b772b9a00a3070e1115bf0aba9be270cd1400992
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-02-21 13:59:18 -0800

    add categories reducer

---
+++ frontend/src/reducers/categories_reducer.js
@@ -0,0 +1,13 @@
+import { RECEIVE_CATEGORIES } from "../actions/emoji_actions";
+
+const CategoriesReducer = (state = {}, action) => {
+  Object.freeze(state)
+  switch(action.type) {
+    case RECEIVE_CATEGORIES:
+      return action.categories
+    default:
+      return state;
+  }
+}
+
+export default CategoriesReducer
--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux"
 import StoryReducer from "./story_reducer"
 import EmojiReducer from "./emoji_reducer"
+import CategoriesReducer from "./categories_reducer"
 
 const EntitiesReducer = combineReducers({
   stories: StoryReducer,
-  emojis: EmojiReducer
+  emojis: EmojiReducer,
+  categories: CategoriesReducer
 })
 
 export default EntitiesReducer
