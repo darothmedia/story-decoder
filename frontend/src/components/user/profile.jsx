2ddf9d92f522bcb4d48672b0d200a5a9378cf761
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-02-22 11:57:35 -0800

    userStories in profile component

--- frontend/src/components/user/profile.jsx
+++ frontend/src/components/user/profile.jsx
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { useParams } from "react-router";
 import { Navigate } from "react-router";
+import { findByUser } from "../../actions/story_actions";
 
 const mSTP = state => ({
   currentUser: state.session.currentUser
 })
 
 const mDTP = dispatch => ({
-
+  findUserStories: userID => dispatch(findByUser(userID))
 })
 
 const Profile = props => {
   const params = useParams()
-  const {currentUser} = props
+  const {currentUser, findUserStories} = props
+  useEffect(() => {
+    findUserStories(params.userID)
+  }, [])
+
   if (!currentUser.id) {
     return(
       <Navigate to='/' />
     )
   }
-
+  
   return(
     <div>
       <h1>Hello, {currentUser.emoji}</h1>
